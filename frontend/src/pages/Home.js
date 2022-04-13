b8f76b8d238f5919a3e142066a57977f1ab09dcd
Dancan Chibole <dancankingstar@gmail.com>
2022-04-13 17:24:25 +0300

    jwt decoding

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import jwt_decode from "jwt-decode";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [person, setPerson] = useState({ name: "", id: "" });
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userDetails = {} } = userLogin;
+  const { access = "" } = userDetails;
+
+  useEffect(() => {
+    if (access) {
+      const { username, user_id } = jwt_decode(access);
+      setPerson({ ...person, name: username, id: user_id });
+    }
+  }, [access, person]);
+
+  return (
+    <>
+      <h2>Hello - {person.name}, Welcome</h2>
+      <h3>Your are ID number - {person.id}</h3>
+    </>
+  );
 };
 
 export default Home;
