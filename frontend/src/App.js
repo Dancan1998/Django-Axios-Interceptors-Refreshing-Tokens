65a7367d22635495e287677d09fc57355768fbef
Dancan Chibole <dancankingstar@gmail.com>
2022-04-13 10:27:17 +0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Header from "./components/Header";
-import PrivateRoute from "./utils/PrivateRoutes";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<PrivateRoute />}>
-          <Route exact path="/" element={<Home />} />
-        </Route>
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
